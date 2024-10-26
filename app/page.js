275dd9cb2de291d5a12d71d8f3e00ae2d93e9641
Hasanat902 <abulhasanat902@gmail.com>
2024-10-26 22:00:54 +0600

    completed design of this app

--- app/page.js
+++ app/page.js
@@ -1,9 +1,54 @@
-
+import Todo from "@/components/Todo";
 
 export default function Home() {
   return (
     <>
-      
+      <form className="flex flex-col items-start gap-2 w-[80%] max-w-[600px] mt-24 px-2 mx-auto">
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter Title"
+          className="px-3 py-2 w-full border-2"
+        />
+        <textarea
+          name="description"
+          placeholder="Enter Description"
+          className="px-3 py-2 w-full border-2"
+        ></textarea>
+        <button
+          type="submit"
+          className="bg-orange-600 py-3 px-8 text-white rounded"
+        >
+          Add Todo
+        </button>
+      </form>
+
+      <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50  dark:text-gray-700">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                ID
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Description
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <Todo />
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
